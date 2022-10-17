af299bc45d063f0d32fdde064c789f27fb4bad92
Fabian Kelvans <fkelvans@gmail.com>
2022-10-17 09:25:53 +0300

    favicon update

--- seo/Head/Head.tsx
+++ seo/Head/Head.tsx
@@ -12,26 +12,26 @@ export interface HeadProps {
 export const Head = (props: HeadProps) => {
     const {
         ogImage = 'https://res.cloudinary.com/dpv0ukspz/image/upload/v1650138958/og-100_mvbgru.jpg',
-        title = 'Portfolio 2022',
-        description = 'Michal Zalobny portfolio 2022 WebGL & GLSL',
+        title = 'Bloggy',
+        description = 'Creative Blog',
     } = props;
 
     return (
         <NextHead>
-            <title>{`Bloggy - ${title}`}</title>
+            <title>{` ${title}`}</title>
     <meta name="description" content={description} />
     <link
     rel="icon"
-    href="https://res.cloudinary.com/dpv0ukspz/image/upload/v1650137433/icon_wghyio.ico"
+    href="/favicon.ico"
     />
 
     <meta property="og:type" content="website" />
-    <meta property="og:title" content={`Bloggy - ${title}`} />
+    <meta property="og:title" content={` ${title}`} />
     <meta property="og:description" content={description} />
     <meta property="og:image" content={ogImage} />
 
     <meta name="twitter:card" content="summary_large_image" />
-    <meta name="twitter:title" content={`Bloggy - ${title}`} />
+    <meta name="twitter:title" content={`${title}`} />
     <meta name="twitter:description" content={description} />
 
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
